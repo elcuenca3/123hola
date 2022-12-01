a9e677b7d7402807c93ecf79b48fe53071f6df1d
elcuenca3 <elcuenca3@utpl.edu.ec>
2022-12-01 14:30:40 -0500

    mascotas taller

---
+++ app/src/main/java/com/example/examen/mascotas.kt
@@ -0,0 +1,11 @@
+package com.example.examen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mascotas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mascotas)
+    }
+}
